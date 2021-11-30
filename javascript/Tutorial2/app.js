a2a66586b093ed3d87392e60dea5fb6b47120b55
osmanismail2021 <osman.ism@live.nl>
2021-11-30 07:06:25 +0100

    update

---
+++ javascript/Tutorial2/app.js
@@ -0,0 +1,10 @@
+console.log('hello')
+console.log('hellooooooo')
+console.log('test js')
+// alert('hello javascript!')
+// alert(<p>adsjaskidasdhaj</p>)
+document.write('<h1>hello js</h1>')
+document.write('<h1>hello jsx1</h1>')
+document.write('intecbrussel"s computer')
+document.writeln('<h1>hello hello</h1>')
+document.getElementById('intecbrussel').innerHTML="Welcome to Intec"
