5ccbeea7431906dd0907ef4d0fa89a70d899a252
osmanismail2021 <osman.ism@live.nl>
2021-12-15 22:55:14 +0100

    update

---
+++ html-and-css/Bootstrap/Tutorial5/login.js
@@ -0,0 +1,5 @@
+let value;
+value = document.links[0]
+value = document.links[0].className = "btn btn-danger"
+
+console.log(value)
--- javascript/Exercises/exercises3.js
+++ javascript/Exercises/exercises3.js
@@ -5,10 +5,38 @@
 //     i = i + 2;
 
 // }
-var x = 2;
+// var x = 2;
 
-while (x < 50){
-    console.log("x degeri : " + x)
-    x = x + 5;
+// while (x < 50){
+//     console.log("x degeri : " + x)
+//     x = x + 5;
 
+// }
+
+
+// var x = 2;
+
+// while (x < 24){
+//     console.log("x degeri : " + x)
+//     x = x + 2;
+// }
+
+var username = "emine"
+var password = "123456"
+var rateLimit=3;
+
+while(rateLimit>0){
+    var askUsername = prompt("enter your username")
+    var askPassword = prompt("Enter your password!")
+    rateLimit--;
+    if(askUsername == username && askPassword == password){
+        console.log('Hosgeldin Emine!')
+        break;
+    }else{
+        console.log(`You've ${rateLimit} attempts left.`)
+    }
 }
+
+document.getElementById('result').innerHTML = username('emine')
+
+
