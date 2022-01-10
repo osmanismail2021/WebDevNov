caf03fcbeef27455b5cea98005e179d7b5b3917e
osmanismail2021 <osman.ism@live.nl>
2022-01-10 16:05:25 +0100

    update

--- javascript/Tutorial1/index.js
+++ javascript/Tutorial1/index.js
@@ -1,14 +1,15 @@
-
-console.log('hello')
-console.log('helloooooooo')
-console.log('test js')
+console.log("hello");
+console.log("helloooooooo");
+console.log("test js");
 //alert('hello javascript!')
 //alert('<p>adsjaskidasdahj</p>')
-document.write("<h1>hello js</h1>")
-document.write('<h1>hello jsx1</h1>')
-document.write('Intecbrussel"s computer')
-document.writeln("<h1>hello hello</h1>")
-document.write('Intecbrussel"s computer')
-document.writeln("<h1>hello hello</h1>")
+document.write("<h1>hello js</h1>");
+document.write("<h1>hello jsx1</h1>");
+document.write('Intecbrussel"s computer');
+document.writeln("<h1>hello hello</h1>");
+document.write('Intecbrussel"s computer');
+document.writeln("<h1>hello hello</h1>");
 
-document.getElementById('intecbrussel').innerHTML="Welcome to Intecbrussel"
+document.getElementById("intecbrussel").innerHTML = "Welcome to Intecbrussel";
+
+
